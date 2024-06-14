6576d9b9d78a8724402a4bd4335130f1f6f0d013
Tiago Becker <84158213+TiagoRBecker@users.noreply.github.com>
2024-06-14 14:49:14 -0300

    Update route.ts

--- src/app/api/payment/route.ts
+++ src/app/api/payment/route.ts
@@ -59,7 +59,7 @@ export async function POST(req: Request, res: Response) {
               expires_in: 108000,
               default_payment_method: "pix",
               accepted_payment_methods: ["pix", "credit_card"],
-              success_url: "http://localhost:3000/sucess",
+              success_url: "https://guedesprod.vercel.app/sucess",
               skip_checkout_success_page: true,
               customer_editable: true,
               billing_address_editable: true,
